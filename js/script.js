b34b51099ff1186de5dff3797021b88ff49932e8
Sherman Leung <skleung@stanford.edu>
2014-01-11 03:23:37 -0800

    updated form

--- js/script.js
+++ js/script.js
@@ -14,12 +14,14 @@ $(document).ready(function(){
     $('.create-room-btn').click(function(){ // when the button is clicked the code executes
 		$('.error').hide(); // reset the error messages (hides them)
 		var error = false; // we will set this true if the form isn't valid
-		var data_string = $('.create-room').serialize(); // Collect data from form
+		var form_string = $('.create-room').serialize(); // Collect data from form
 		var randomKey = getKey();
+		var data_string = form_string+"&roomkey="+randomKey;
+		alert("data_string = "+data_string);
 		$.ajax({
 			type: "POST",
 			url: $('#create-room').attr('action'),
-			data: data_string+"&key="+randomKey,
+			data: data_string,
 			timeout: 6000,
 			error: function(request,error) {
 				if (error == "timeout") {
@@ -39,5 +41,19 @@ $(document).ready(function(){
 			}
 		});
 		return false; // stops user browser being directed to the php file
+	});
+
+
+	//Scrolling animation
+	$(".scroll").click(function(event){
+		event.preventDefault();
+		var full_url = this.href;
+		var parts = full_url.split("#");
+		var trgt = parts[1];
+		var target_offset = $("#" + trgt).offset();
+		var target_top = target_offset.top;
+
+		//goto that anchor by setting the body scroll top to anchor top
+		$('html, body').animate({scrollTop: target_top}, 400);
 	});
 });
